b5ef51e864db3266bdeecddfe6a4b2c5d2f02b6f
Alhadis <gardnerjohng@gmail.com>
2016-02-23 19:33:01 +1100

    Add multiline cell handling to table function

--- multisort.js
+++ multisort.js
@@ -7,7 +7,7 @@ let data = fs.readFileSync("list.tsv")
 	.toString()
 	.split(/\n+/g)
 	.filter(e => e)
-	.map(e => e.split(/\t/g));
+	.map(e => e.replace(/\\n/g, "\n").split(/\t/g));
 
 
 let str = table(data, {
@@ -29,31 +29,85 @@ function table(values, options){
 	
 	
 	/** Determine the maximum size of each column */
-	let maxLengths = [];
+	let maxLengths    = [];
+	let numColumns    = 0;
 	for(let row of values){
 		
-		for(let i = 0, l = row.length; i < l; ++i){
+		/** Record the maximum number of columns in case cell-count is inconsistent */
+		let l = row.length;
+		if(l > numColumns)
+			numColumns = l;
+		
+		for(let i = 0; i < l; ++i){
 			let cell = row[i];
 			maxLengths[i] = Math.max(cell.length, maxLengths[i] || 0);
 		}
 	}
 	
 	/** Ascertain the minimum width required to fit the whole table */
-	let minWidth = maxLengths.reduce((a, b) => a + b + padding);
+	const minWidth       = maxLengths.reduce((a, b) => a + b + padding);
 	
 	/** If a desired width was specified, calculate a multiplier */
-	let sizeModifier = width ? (width / minWidth) : 1;
+	const sizeModifier   = width ? (width / minWidth) : 1;
 	
+
+	/** Number of extra rows inserted to contain a multi-line value */
+	let buffer = 0;
 	
 	/** Start spitting out rows */
 	let s = "";
-	for(let row of values){
+	for(let r = 0, rowCount = values.length; r < rowCount; ++r){
+		let row = values[r];
 		
 		for(let i = 0, l = row.length; i < l; ++i){
-			let cell = row[i];
-			s += " ".repeat(paddingLeft) + cell + " ".repeat(((paddingRight + maxLengths[i]) * sizeModifier) - cell.length);
+			let text = row[i] || "";
+			
+			
+			/** This cell's content contains line-breaks */
+			if(/\n/.test(text)){
+				const lines   = text.split(/\n/g);
+				let numLines  = lines.length;
+				let injectOffset = 1;				
+				
+				/** Replace the original cell's content with the first line, and remove it from the lines to be inserted */
+				text = lines.shift();
+				--numLines;
+				
+				
+				/** Do we need more space? */
+				if(numLines > buffer){
+					const newRows = [];
+					const diff    = numLines - buffer;
+					
+					/** Create new rows of blank cells */
+					for(let L = 0; L < diff; ++L)
+						newRows.push(Array(numColumns));
+					
+					/** Inject them between the rest of our rows */
+					values.splice.apply(values, [r + injectOffset + buffer, 0, ...newRows]);
+					rowCount = values.length;
+					buffer   = numLines;
+				}
+				
+				
+				/** Inject each split line into the extra/empty rows below */
+				for(let L = 0; L < numLines; ++L){
+					values[r + L + 1][i] = lines[L];
+				}
+			}
+			
+			
+			/** Add the cell to the output string */
+			s +=
+				" ".repeat(paddingLeft) +
+				text +
+				" ".repeat(((paddingRight + maxLengths[i]) * sizeModifier) - text.length);
 		}
+		
 		s += "\n";
+		
+		/** Decree the rowBuffer count */
+		buffer && --buffer;
 	}
 	
 	return s;
